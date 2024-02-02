ef01f00c33d9528aca9351047809f8bf1c023917
AYMEN <gherdaineaymen1995@gmail.com>
2024-02-01 20:38:52 -0500

    Separate frontend form backend

--- backend/auth/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const { JWT_SECRET } = process.env;
-
-const generateToken = (data) => {
-  return jwt.sign({ data }, JWT_SECRET, { expiresIn: "2d" });
-};
-
-module.exports = { generateToken };
